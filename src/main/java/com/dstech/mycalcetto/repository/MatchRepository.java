7744e9c517b56a17e8d1a482426e62a26739c24f
retlawireinarts <stranieriw@gmail.com>
2021-11-10 14:17:15 +0100

    Esposizione metodi del repository come servizi REST

--- src/main/java/com/dstech/mycalcetto/repository/MatchRepository.java
+++ src/main/java/com/dstech/mycalcetto/repository/MatchRepository.java
@@ -5,11 +5,16 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 import com.dstech.mycalcetto.entity.Match;
 
-@Repository
+/**
+ * Usando l'annotazione {@link RepositoryRestResource} al posto di {@link JpaRepository},
+ * Spring crea automaticamente i servizi rest per tutti i metodi CRUD esposti da questo repository
+ */
+@RepositoryRestResource( collectionResourceRel = "matchs", path = "matchs")
 public interface MatchRepository extends PagingAndSortingRepository<Match, Long> {
 	List<Match> findByisPrivateFalseAndDateTimeAfter(LocalDateTime dateOfYesterday);
 	
