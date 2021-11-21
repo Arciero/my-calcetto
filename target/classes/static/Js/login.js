1e51c97f86fa939300242e59cd06c00c1caf8cb6
Federico-95 <federico.desimone95@gmail.com>
2021-11-21 16:17:35 +0100

    creazione del branch su cui faremo il merge

---
+++ target/classes/static/Js/login.js
@@ -0,0 +1,5 @@
+alert("bella pe te");
+function changeDiv (idDiv1, idDiv2) {
+    document.getElementById(idDiv1).style.display="none";
+    document.getElementById(idDiv2).style.display="block";
+}
